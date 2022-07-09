aef282f4473aa9577ea58ad06f0bb8619a790130
xk <xkisxk@gmail.com>
2022-07-09 19:24:24 +0800

    Added reply with nearest bin location function

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ from dotenv import load_dotenv
 import json
 import os
 import requests
+import logic, loc
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # functions
-
-
 async def hello(update, context):
     await update.message.reply_text("Hi welcome to GoGreen!")
+
+async def bin(update, context):
+    # reply nearest bin location
+    user_location = update.message.location
+    lat, long = logic.findNearestBin(loc.LOCATIONS, user_location.latitude, user_location.longitude)
+    await update.message.reply_location(latitude=lat, longitude=long)
 
 # Building the bot
 bot = ApplicationBuilder().token(BOT_TOKEN).build()
 
 # Add handlers
 bot.add_handler(CommandHandler("hello", hello))
+bot.add_handler(MessageHandler(filters.LOCATION, bin))
 bot.run_polling()
